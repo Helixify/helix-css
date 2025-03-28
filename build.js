fbff32a76ef07a441bc15cd3b4a086c39c23862e
andre.aguiar <andreluizmagjr@outlook.com>
2025-03-28 15:01:46 -0300

    Fisnish helper

--- build.js
+++ build.js
@@ -22,7 +22,7 @@ const __dirname = dirname(__filename);
 // 	"spacing.scss", 
 // 	"transition.scss", 
 // 	"typography.scss"];
-const sassFiles = ["main.scss", "grid.scss"];
+const sassFiles = ["main.scss", "helper.scss"];
 
 const outputDir = join(__dirname, "dist");
 
