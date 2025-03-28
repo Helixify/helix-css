f9bf07a5c64da880bd7ae6e639e2076bb9631f8f
andre.aguiar <andreluizmagjr@outlook.com>
2025-03-28 10:54:21 -0300

    Finish display grid

--- build.js
+++ build.js
@@ -22,7 +22,7 @@ const __dirname = dirname(__filename);
 // 	"spacing.scss", 
 // 	"transition.scss", 
 // 	"typography.scss"];
-const sassFiles = ["main.scss", "gap.scss"];
+const sassFiles = ["main.scss", "grid.scss"];
 
 const outputDir = join(__dirname, "dist");
 
