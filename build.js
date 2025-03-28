ba5cde2a6cabec2aeb6d61ce4206052696e2dc7a
andre.aguiar <andreluizmagjr@outlook.com>
2025-03-28 16:25:16 -0300

    Init layout component

--- build.js
+++ build.js
@@ -22,7 +22,7 @@ const __dirname = dirname(__filename);
 // 	"spacing.scss", 
 // 	"transition.scss", 
 // 	"typography.scss"];
-const sassFiles = ["main.scss", "helper.scss"];
+const sassFiles = ["main.scss", "layout.scss"];
 
 const outputDir = join(__dirname, "dist");
 
