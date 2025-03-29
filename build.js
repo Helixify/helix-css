d254a2e159bd1aa835d3f5622168875fe5c7b3a2
AndreLuizMagJr <andreluizmagjr@outlook.com>
2025-03-29 10:25:30 -0300

    Finish spacing

--- build.js
+++ build.js
@@ -22,7 +22,7 @@ const __dirname = dirname(__filename);
 // 	"spacing.scss",
 // 	"transition.scss",
 // 	"typography.scss"];
-const sassFiles = ["main.scss", "position.scss"];
+const sassFiles = ["main.scss", "spacing.scss"];
 
 const outputDir = join(__dirname, "dist");
 
